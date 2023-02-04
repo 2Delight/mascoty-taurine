cc0ddeadaed170c361a8faf30495b94527062f0f
Mmmr7 <ol-sidorenkov@mail.ru>
2023-02-05 00:59:23 +0300

    cmds move

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+    windows_subsystem = "windows",
 )]
 
 extern crate log;
@@ -15,71 +15,14 @@ mod config;
 mod input;
 mod mascot;
 mod utils;
+mod commands;
 
 use crate::config::import_config;
-use crate::input::{Devices, get_cams, set_camera, get_devices};
+use crate::commands::*;
+use crate::input::{get_cams, set_camera, get_devices};
 
-use config::Config;
 use log::{debug, error, info, warn};
 use simple_logger::SimpleLogger;
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn get_mascot(state: tauri::State<Devices>) -> mascot::Mascot {
-    mascot::get_mascot(&state)
-}
-
-#[tauri::command]
-fn get_cameras() -> Result<Vec<String>, String> {
-    match get_cams() {
-        Ok(val) => Ok(
-            val.iter()
-            .map(
-                |info: &nokhwa::utils::CameraInfo| {
-                    info.description().to_string()
-                },
-            ).collect(),
-        ),
-        Err(err) => Err(err.to_string()),
-    }
-}
-
-#[tauri::command]
-fn select_camera(index: i32, conf: Config, state: tauri::State<Devices>) -> Result<(), String> {
-    if index < 0 {
-        return Err("Index has to be greater than -1".to_string());
-    }
-    let cams = get_cams().unwrap();
-    let ind  = cams[index as usize].index().clone();
-    
-    let cam = match set_camera(cams, ind, &conf) {
-        Ok(cam) => cam,
-        Err(err) => {
-            return Err(err.to_string())
-        },
-    };
-
-    state.set_camera(conf, cam);
-
-    Ok(())
-}
-
-#[tauri::command]
-fn set_fps(fps: i32, state: tauri::State<Devices>) -> Result<(), String> {
-    let mut conf = state.get_conf();
-    if fps < 1 || fps > 30 {
-        return Err("FPS has to be greater than 0 and lesser than 31!".to_string());
-    }
-
-    conf.camera.fps = fps as u32;
-    state.set_camera(conf, set_camera(
-        get_cams().unwrap(),
-        state.get_camera_index(),
-        &conf,
-    ).unwrap());
-
-    Ok(())
-}
 
 fn main() {
     match SimpleLogger::new()
