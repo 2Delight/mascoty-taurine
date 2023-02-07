a1a12687e6e000a0d544b1cb793b1948fa7d2b19
Аленов Михаил <banan.bananev@gmail.com>
2023-02-07 21:21:52 +0300

    list of cams now actually works

--- src/components/settings/MicSelection.tsx
+++ src/components/settings/MicSelection.tsx
@@ -3,6 +3,48 @@ import { SelectChangeEvent, Select, MenuItem } from '@mui/material';
 import React from 'react';
 import { menuGray } from '../../utils/Colors';
 
+function getMicrophoneVolume(): number {
+    let ans = 0.0;
+    navigator.mediaDevices.getUserMedia({
+        audio: {
+            echoCancellation: true
+        }
+    }).then((stream: MediaStream) => {
+        ans = getMicrophoneInfo(stream);
+    }).catch(
+        function (err) {
+            console.log(err);
+        }
+    );
+
+    return ans;
+}
+
+function getMicrophoneInfo(stream: MediaStream): number {
+    const audioContext = new AudioContext();
+    const audioSource = audioContext.createMediaStreamSource(stream);
+
+    const analyser = audioContext.createAnalyser();
+    analyser.fftSize = 512;
+    analyser.minDecibels = -127;
+    analyser.maxDecibels = 0;
+    analyser.smoothingTimeConstant = 0.4;
+    audioSource.connect(analyser);
+
+    const volumes = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(volumes);
+
+    let volumeSum = 0;
+    for (const volume of volumes) {
+        volumeSum += volume;
+    }
+
+    return volumeSum / volumes.length;
+}
+
+
+
+
 export default function MicSelection() {
     const [age, setAge] = React.useState('');
 
