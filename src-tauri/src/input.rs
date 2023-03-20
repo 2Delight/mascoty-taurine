5b08852ee2ff5a896094889849845000bbdcb901
Mmmr7 <ol-sidorenkov@mail.ru>
2023-03-20 14:35:39 +0100

    mike

--- src-tauri/src/input.rs
+++ src-tauri/src/input.rs
@@ -2,7 +2,7 @@ use crate::config::{CameraConfig, Config};
 use crate::panic_error;
 
 use std::io::Read;
-use std::sync::{Mutex, MutexGuard, PoisonError};
+use std::sync::{Mutex, MutexGuard, PoisonError, mpsc::{Sender, Receiver}};
 // use std::cell::RefCell;
 
 use nokhwa::pixel_format::*;
@@ -12,11 +12,14 @@ use nokhwa::utils::{
 };
 use nokhwa::{query, Camera, NokhwaError};
 
+use portaudio::{DeviceIndex, DeviceInfo, Error, PortAudio};
+
 use log::{debug, error, info, warn};
 
 pub struct Devices {
     pub camera: Mutex<Camera>,
-    conf: Mutex<Config>,
+    config: Mutex<Config>,
+    pub microphone: Mutex<Receiver<Vec<f32>>>,
 }
 
 unsafe impl Sync for Devices {}
@@ -31,7 +34,7 @@ impl Devices {
             config
         );
 
-        let mut conf_guard = match self.conf.lock() {
+        let mut conf_guard = match self.config.lock() {
             Ok(val) => val,
             Err(_) => {
                 return Err("Cannot get value from config mutex".to_string());
@@ -46,6 +49,20 @@ impl Devices {
             }
         };
         *cam_guard = camera;
+
+        Ok(())
+    }
+
+    pub fn set_up_microphone(&self, microphone: Receiver<Vec<f32>>) -> Result<(), String> {
+        info!("Setting the microphone");
+
+        let mut mike_guard = match self.microphone.lock() {
+            Ok(val) => val,
+            Err(_) => {
+                return Err("Cannot get value from config mutex".to_string());
+            }
+        };
+        *mike_guard = microphone;
 
         Ok(())
     }
@@ -74,6 +91,66 @@ impl Devices {
 
 pub struct Input {}
 
+pub fn get_mikes(pa: &PortAudio) -> Result<Vec<(DeviceIndex, DeviceInfo)>, Error> {
+    let devices: Vec<(portaudio::DeviceIndex, portaudio::DeviceInfo)> = pa
+        .devices()?
+        .filter(|device| device.is_ok())
+        .map(|device| device.unwrap())
+        .filter(|device| device.1.max_input_channels > 0)
+        .collect();
+
+    if devices.len() == 0 {
+        return Err(Error::InvalidDevice);
+    }
+
+    Ok(devices)
+}
+
+pub fn set_mike<'a>(
+    index: usize,
+    pa: &PortAudio,
+) -> Result<std::sync::mpsc::Receiver<Vec<f32>>, Error> {
+    const CHANNELS_NUMBER: i32 = 1;
+    const FRAMES_NUMBER_PER_BUFFER: u32 = 256;
+    const CHOSEN: usize = 0;
+
+    let devices = get_mikes(pa)?;
+    let input_params = portaudio::StreamParameters::<f32>::new(
+        devices[index].0,
+        CHANNELS_NUMBER,
+        true,
+        devices[index].1.default_low_input_latency,
+    );
+
+    let input_settings = portaudio::InputStreamSettings::new(
+        input_params,
+        devices[CHOSEN].1.default_sample_rate,
+        FRAMES_NUMBER_PER_BUFFER,
+    );
+
+    let (sender, receiver) = std::sync::mpsc::channel::<Vec<f32>>();
+    let callback = move |portaudio::InputStreamCallbackArgs { buffer, .. }| match sender
+        .send(buffer.to_vec())
+    {
+        Ok(_) => portaudio::Continue,
+        Err(_) => portaudio::Complete,
+    };
+
+    let mut stream = pa.open_non_blocking_stream(input_settings, callback)?;
+
+    stream.start()?;
+    println!("{}", stream.is_active()?);
+    println!("{}", stream.is_stopped()?);
+
+    // loop {
+    //     let buffer = receiver.recv().unwrap();
+    //     println!("{}", sum(buffer));
+    //     std::thread::sleep(std::time::Duration::from_millis(500))
+    // }
+
+    Ok(receiver)
+}
+
 pub fn get_cams() -> Result<Vec<CameraInfo>, NokhwaError> {
     debug!("Getting camera list");
 
@@ -87,7 +164,7 @@ pub fn get_cams() -> Result<Vec<CameraInfo>, NokhwaError> {
     Ok(cams)
 }
 
-pub fn set_camera(index: CameraIndex, config: &CameraConfig) -> Result<Camera, NokhwaError> {
+pub fn set_cam(index: CameraIndex, config: &CameraConfig) -> Result<Camera, NokhwaError> {
     info!("Setting the camera №{:?} with config {:?}", index, config);
 
     let format_type = RequestedFormatType::Exact(CameraFormat::new_from(
@@ -104,16 +181,16 @@ pub fn set_camera(index: CameraIndex, config: &CameraConfig) -> Result<Camera, N
     debug!("Openning stream");
     camera.open_stream().unwrap();
 
-
     Ok(camera)
 }
 
-pub fn get_devices(config: Config, camera: Camera) -> Devices {
+pub fn get_devices(config: Config, camera: Camera, microphone: Receiver<Vec<f32>>) -> Devices {
     debug!("Initializing devices");
 
     Devices {
         camera: Mutex::new(camera),
-        conf: Mutex::new(config),
+        config: Mutex::new(config),
+        microphone: Mutex::new(microphone),
     }
 }
 
@@ -130,10 +207,13 @@ pub fn get_input(devices: &Devices) -> Result<Input, NokhwaError> {
         0,
     );
     camera.write_frame_to_buffer::<RgbFormat>(&mut img).unwrap();
-    let img = image::io::Reader::new(std::io::Cursor::new(&img)).with_guessed_format().unwrap().decode().unwrap();
+    let img = image::io::Reader::new(std::io::Cursor::new(&img))
+        .with_guessed_format()
+        .unwrap()
+        .decode()
+        .unwrap();
 
     // debug!("{}", frame.source_frame_format());
-    
 
     // debug!("Decoding image");
     // let mut img = Vec::new();
@@ -144,7 +224,7 @@ pub fn get_input(devices: &Devices) -> Result<Input, NokhwaError> {
 
     // frame.decode_image_to_buffer::<RgbFormat>(&mut img).unwrap();
 
-    let model = &mut devices.conf.lock().unwrap().model;
+    let model = &mut devices.config.lock().unwrap().model;
     model.set_eval();
 
     let output = tch::vision::imagenet::load_image_and_resize224_from_memory(img.as_bytes())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,7 +19,7 @@ mod utils;
 
 use crate::commands::*;
 use crate::config::import_config;
-use crate::input::{get_cams, get_devices, set_camera};
+use crate::input::{get_cams, get_devices, set_cam, set_mike};
 
 use log::{debug, error, info, warn};
 use simple_logger::SimpleLogger;
@@ -39,26 +39,36 @@ fn main() {
 
     debug!("Getting default camera index");
     let cam = panic_error!(
-        set_camera(
+        set_cam(
             get_cams().unwrap()[0].index().clone(),
             &conf.camera,
         ),
         "setting up camera",
     );
 
+    let pa = portaudio::PortAudio::new().unwrap();
+
+    debug!("Getting default micro");
+    let mike = panic_error!(
+        set_mike(0, &pa), "setting up microphone"
+    );
+
     debug!("Getting devices");
-    let devices = get_devices(conf, cam);
+    let devices = get_devices(conf, cam, mike);
 
     mascot::get_mascot(&devices);
 
     debug!("Building the app");
     tauri::Builder::default()
         .manage(devices)
+        .manage(pa)
         .invoke_handler(tauri::generate_handler![
             get_mascot,
             get_cameras,
             select_camera,
-            set_config,
+            set_camera_config,
+            get_microphones,
+            select_microphone,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
